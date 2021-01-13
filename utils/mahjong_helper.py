e01238a896639d4cc60db31d7f19c1ab862eee2f
confusedbread <confusedbread@DESKTOP-RV29LQQ.localdomain>
2021-01-12 19:03:27 -0600

    Implemented Kokushi and Chiitoi Shanten Calculations

---
+++ utils/mahjong_helper.py
@@ -0,0 +1,30 @@
+def parse_hand_from_string(hand_str):
+  hand = {}
+  tiles = []
+  tile_count = 0
+
+  for char in hand_str:
+    if valid_suit(hand_str, char):
+      if valid_tiles(hand_str, char, tiles):
+        hand[char] = tiles
+        tiles = []
+    else:
+      tiles.append(int(char))
+      tile_count += 1
+
+  if tile_count != 13:
+    raise Exception("Invalid Number of tiles {}. Expected 13".format(tile_count))
+
+  return hand
+
+def valid_suit(hand_str, suit):
+  return (suit == 'm' or suit == 's' or suit == 'p' or suit == 'z')
+
+def valid_tiles(hand_str, suit, tiles):
+  max_tile = 8 if suit == 'z' else 10
+  
+  for tile in tiles:
+    if 0 >= tile or tile >= max_tile:
+      raise Exception("Invalid Hand {}. Found in {}{}".format(hand_str, tiles, suit))
+
+  return True
--- utils/shanten.py
+++ utils/shanten.py
@@ -1,5 +1,4 @@
-from utils import mahjong_helper
-
+from mahjong_helper import parse_hand_from_string
 
 def calculate_shanten(hand):
   pass
@@ -10,16 +9,12 @@ def chiitoi_shanten(hand):
   for suit, tiles in hand.items():
     temp = {x: 0 for x in range(1, 10)}
     for tile in tiles:
-      if tile not in temp:
-        
-        pass
-      else:
-        temp[tile] += 1
+      temp[tile] += 1
 
     for key, value in temp.items():
       if value >= 2:
         pair_count += 1
-  
+
   return 6 - pair_count
 
 def kokushi_shanten(hand):
@@ -28,14 +23,21 @@ def kokushi_shanten(hand):
   for suit, tiles in hand.items():
     if suit == 'z':
       for i in range(1,7):
+        if i in tiles:
+          unique_count += 1
     else:
-      if '1' in tiles:
+      if 1 in tiles:
         unique_count += 1
-      if '9' in tiles:
+      if 9 in tiles:
         unique_count += 1
+  
+  return 13 - unique_count
 
-chiitoi_hand = "1133m45s699p112z"
+
+chiitoi_hand = "1133m45s1699p112z"
 my_hand = parse_hand_from_string(chiitoi_hand)
 print(my_hand)
-shanten = chiitoi_shanten(my_hand)
-print(shanten)
+shanten_pears = chiitoi_shanten(my_hand)
+print(shanten_pears)
+shanten_kekw = kokushi_shanten(my_hand)
+print(shanten_kekw)
