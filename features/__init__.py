630e7b8179e4c2a12d4bee4a6b6b14f088cad1f3
FC <chiang2.felix@gmail.com>
2021-02-09 15:59:03 -0600

    Converted Japanese Module into cog

--- features/__init__.py
+++ features/__init__.py
@@ -1,6 +1,8 @@
 from client.bot import breadBot as bot
 from features.role_react import ReactionRoles
 from features.role_live_now import LiveNow
+from features.japanese_support import JapaneseSupport
 
 bot.add_cog(ReactionRoles(bot))
 bot.add_cog(LiveNow(bot))
+bot.add_cog(JapaneseSupport(bot))
