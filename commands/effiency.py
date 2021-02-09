35165606a374b83791c3714257a386057399de41
FC <chiang2.felix@gmail.com>
2021-02-09 11:58:13 -0600

    refactored commands to utilize commands decorator

--- commands/effiency.py
+++ commands/effiency.py
@@ -1,17 +1,12 @@
-from client.bot import log
+from commands import bot
 from utils.shanten import calculate_shanten
 from utils.hand_to_emoji import hand_to_emoji
 
-async def effiency(bot, message):
-    
-    args = message.content.split()
-
-    if len(args) != 2:
-        await message.channel.send('Invalid Number of elements')
-        return
-
-    hand = args[1]
+@bot.command(name='eff')
+async def effiency(ctx, hand=None):
+    if not hand:
+        await ctx.send("Invalid Hand {}".format(hand))
 
     emoji_hand = hand_to_emoji(hand)
     shanten = calculate_shanten(hand)
-    await message.channel.send("{} \n Shanten-{}".format(emoji_hand, shanten))
+    await ctx.send("{} \n Shanten-{}".format(emoji_hand, shanten))
